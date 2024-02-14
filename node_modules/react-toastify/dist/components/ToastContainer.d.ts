fca5de3de2eab61744a0e91cf7812103eaca706f
urstrulynishkarsh <206301010@gkv.ac.in>
2024-02-14 10:19:06 +0530

    Remove node_modules from tracking

--- node_modules/react-toastify/dist/components/ToastContainer.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { ToastContainerProps } from '../types';
-export declare const defaultProps: ToastContainerProps;
-export declare function ToastContainer(props: ToastContainerProps): React.JSX.Element;
