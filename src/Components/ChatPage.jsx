beaecd983eeea37198cfa6258ad9891102fd5ab8
urstrulynishkarsh <206301010@gkv.ac.in>
2024-03-08 10:03:33 +0530

    envdoneaws

--- src/Components/ChatPage.jsx
+++ src/Components/ChatPage.jsx
@@ -12,7 +12,7 @@ import toast from 'react-hot-toast';
 
 
 
-const socket = io('ws://localhost:4000', { transports: ['websocket'] });
+const socket = io('ws://socketio-env.eba-3jitqgnh.ap-south-1.elasticbeanstalk.com/', { transports: ['websocket'] });
 
 const ChatPage = () => {
     
