0065a4bd06abca2a27029cdb5753f3384cb3aa14
urstrulynishkarsh <206301010@gkv.ac.in>
2024-05-12 21:14:26 +0530

    done

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import {Navigate, useNavigate} from "react-router-dom";
 
 import {useAuth} from "../Context/UserAuthContext";
 
-export function PrivateRoute({children} :{children : ReactNode}){
+export function PrivateRoute({children}){
     const { roomDetail }  = useAuth()
     return  roomDetail?<>{children}</> : <Navigate to="/"/>
 
