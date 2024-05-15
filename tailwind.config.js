d85e5f0611cd66b20b4a815d209e30f208064a3e
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-05-16 04:47:24 +0530

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -130,14 +130,7 @@ module.exports = {
         maxContent: "1260px",
         maxContentTab: "650px"
       },
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-        '3xl': '1780px', // new breakpoint
-      },
+   
     },
   },
   plugins: [],
