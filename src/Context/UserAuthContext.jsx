835caf6bfcb23e5f0ef353f715bca42707ecdd94
Swarup Bhise <swarupbhise420@gmail.com>
2024-05-18 13:48:40 +0530

    Bug #122 solve the user redirect and the popup error

--- src/Context/UserAuthContext.jsx
+++ src/Context/UserAuthContext.jsx
@@ -1,22 +1,21 @@
-import {createContext, useContext, useState} from "react";
-import {ReactNode} from "react";
+import { createContext, useContext, useState } from "react";
+import { ReactNode } from "react";
 
-type useAuthProviderProps = {
-    children : ReactNode
+const userAuthContext = createContext();
+
+export function useAuth() {
+  return useContext(userAuthContext);
 }
 
-const userAuthContext  = createContext()
+export function UseAuthProvider({ children }) {
+  const [roomDetail, setRoomDetail] = useState({
+    username: "",
+    room: "",
+  });
 
-export function useAuth(){
-    return useContext(userAuthContext)
-}
-
-export function UseAuthProvider({children} : useAuthProviderProps){
-    const [roomDetail, setRoomDetail] = useState("")
-
-    return(
-        <userAuthContext.Provider value={{setRoomDetail, roomDetail}}>
-            {children}
-        </userAuthContext.Provider>
-    )
+  return (
+    <userAuthContext.Provider value={{ setRoomDetail, roomDetail }}>
+      {children}
+    </userAuthContext.Provider>
+  );
 }
