6384d41c742463ce0ab81443145e6a31df53b0a0
urstrulynishkarsh <206301010@gkv.ac.in>
2024-05-18 09:35:50 +0530

    done

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
     reverse:        false,  // reverse the tilt direction
     max:            100,     // max tilt rotation (degrees)
     perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:          5.5,    // 2 = 200%, 1.5 = 150%, etc..
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
     speed:          1000,   // Speed of the enter/exit transition
     transition:     true,   // Set a transition on enter/exit.
     axis:           null,   // What axis should be disabled. Can be X or Y.
