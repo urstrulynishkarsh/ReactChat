48128ae57700a1a04e6b4fdf175b772c25d3e3c6
Swarup Bhise <swarupbhise420@gmail.com>
2024-05-20 23:22:47 +0530

    feat #146 :voice Listener Popup Box and Resolving Message Overwriting Issue

---
+++ src/Components/MicroPhone.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MicroPhone() {
+  return (
+    <div class="mic">
+      <i class="mic-icon"></i>
+      <div class="mic-shadow"></div>
+    </div>
+  );
+}
