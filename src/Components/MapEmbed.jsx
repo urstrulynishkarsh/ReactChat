5219776e9a193b236516cf62d4253a9651c3ce1b
Sudipto Das <dassudipto200@gmail.com>
2024-06-01 15:47:30 +0530

    making additional changes

--- src/Components/MapEmbed.jsx
+++ src/Components/MapEmbed.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const MapEmbed = ({ url}) => {
 
   return (
     <div className="mapembed">
-    <iframe src={`${url}&hl=es;z=14&output=embed`} width="300" height="200"></iframe>
+    <iframe src={`${url}&hl=es;z=14&output=embed`} width="300" height="200" title='map'></iframe>
     </div>
   );
 };
