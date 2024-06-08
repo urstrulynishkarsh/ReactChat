0355cb0b520b3638252d9ddfa1d1c106efa5225f
Siddheya Kulkarni <siddheyakul@gmail.com>
2024-06-08 17:52:46 +0530

    enhanced chat interface ui

--- src/Components/MessageTemplate.jsx
+++ src/Components/MessageTemplate.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './MessageTemplate.css'
+const MessageTemplate = ({ username, createdAt, message, darkMode }) => {
+  const limitedUsername = username.length > 10 ? username.slice(0, 10) + '...' : username;
 
-const MessageTemplate = ({ username, createdAt, message,darkMode }) => {
-  const limitedUsername = username.length> 10 ? username.slice(0, 10) + '...' : username;
   return (
-    <div className="message">
-        <p>
+    <div className={`message-container ${darkMode ? 'dark' : 'light'}`}>
+      <div className="message-box">
+         <p>
             <span className={`${darkMode ? 'text-white' : ' text-black'}  message__name`}>{limitedUsername}</span>
-            <span className="message__meta">{createdAt}</span>
+            <span className={`${darkMode ? 'text-white' : ' text-gray'} message__meta`}>{createdAt}</span>
         </p>
-        <p className={`${darkMode ? 'text-white' : ' text-black'}`}>{message}</p>
-  </div>
-  )
-}
+        <p className="message-text">{message}</p>
+        <div className="message-arrow"></div>
+      </div>
+    </div>
+  );
+};
 
-export default MessageTemplate
+export default MessageTemplate;
